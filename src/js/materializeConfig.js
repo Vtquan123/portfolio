b4c0f2e922b557b7c17dd12ca9b369cd7fcf9052
Vtquan123 <vtquan124@gmail.com>
2020-12-07 23:45:54 +0700

    [Updated] - Skills with responsive and fixed some responsive errors

--- src/js/materializeConfig.js
+++ src/js/materializeConfig.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let elems = document.querySelectorAll(".sidenav");
   let instance = M.Sidenav.init(elems);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let elems = document.querySelectorAll(".collapsible");
+  let instances = M.Collapsible.init(elems);
+});
